f1cb07cdb203dbd76433c0fe967e11ee8fac411d
Carol Enache <enache.carol@gmail.com>
2018-04-04 21:56:51 +0100

    create form and import styles

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,7 +3,11 @@ import React from 'react';
 class StorePicker extends React.Component {
     render() {
         return (
-            <p>Hello!</p>
+            <form className='store-selector'>
+                <h2>Please Enter The Store</h2>
+                <input type='text' placeholder='Store Name'/>
+                <button type='submit'>Visit Store</button>
+            </form>
         );
     }
 }
